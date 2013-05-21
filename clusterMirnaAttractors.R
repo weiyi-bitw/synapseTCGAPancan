7451e39314b1166b758438b1a16766c5cabe70ad
weiyi_gisl <wc2302@columbia.edu>
2013-05-21 17:41:13 -0400

    cluster and consensus

--- clusterMirnaAttractors.R
+++ clusterMirnaAttractors.R
@@ -12,6 +12,10 @@ analysisRepo <- getRepo("weiyi-bitw/synapseTCGAPancan")
 sourceRepoFile(analysisRepo, "sourceScripts/clusterAttractorsFromSynapse.R")
 clusterLink <- getPermlink(analysisRepo, "sourceScripts/clusterAttractorsFromSynapse.R")
 
+sourceRepoFile(analysisRepo, "sourceScripts/createConsensusFromSynapse.R")
+consensusLink <- getPermlink(analysisRepo, "sourceScripts/createConsensusFromSynapse.R")
+
+
 # miRNA attractor matrices Synapse ID
 synIDs <- c(
 	"syn1875859",	#BLCA
@@ -33,6 +37,7 @@ miRNAParentID <- "syn1875845"
 
 used <- list(
 	list(url=clusterLink, name=basename(clusterLink), wasExecuted=TRUE)
+	list(url=consensusLink, name=basename(consensusLink), wasExecuted=TRUE)
 )
 
 for(s in synIDs){
@@ -45,12 +50,24 @@ dir.create(resultDir)
 
 alist <- clusterAttractorsFromSynapse(synIDs, numGenes=5, strength.pos=3, datasetTags=datasetTags, tempDir=tmpDir)
 
-resultFile <- file.path(resultDir, "attractorClusters.mirna.rda")
-save(alist, file=resultFile)
-activity <- Activity(name="Attractor clustering, NG=5, Str=3rd MI", used=used)
-resultFile <- File(resultFile, synapseStore=TRUE, parentId=miRNAParentID)
-generatedBy(resultFile) <- activity
-resultFile <- storeEntity(resultFile)
-activity <- generatedBy(resultFile)
+synIDList <- as.list(synIDs)
+names(synIDList) <- datasetTags
+attractome <- createConsensusFromSynapse(alist, synIDList, minGenes=3, tempDir=tmpDir)
 
+resultFile.cluster <- file.path(resultDir, "attractorClusters.mirna.rda")
+save(alist, file=resultFile.cluster)
+resultFile.consensus <- file.path(resultDir, "attractome.mirna.rda")
+save(attractome, file=resultFile.consensus)
+
+activity <- Activity(name="Attractor clustering, NG=5, Str=3rd MI", used=used)
+
+resultFile.cluster <- File(resultFile.cluster, synapseStore=TRUE, parentId=miRNAParentID)
+generatedBy(resultFile.cluster) <- activity
+resultFile.cluster <- storeEntity(resultFile.cluster)
+activity <- generatedBy(resultFile.cluster)
+
+resultFile.consensus <- File(resultFile.consensus, synapseStore=TRUE, parentId=miRNAParentID)
+generatedBy(resultFile.consensus) <- activity
+resultFile.consensus <- storeEntity(resultFile.consensus)
+activity <- generatedBy(resultFile.consensus)
 
---
+++ sourceScripts/createConsensusFromSynapse.R
@@ -0,0 +1,75 @@
+require(cafr)
+require(synapseClient)
+
+createConsensusFromSynapse <- function(alist, synIDList, minScore=0.5, minGenes=10, th=6, tempDir = tempdir()){
+  attractome <- list()
+  cMat <- list()
+  env <- new.env()
+  nas <- length(alist)
+  cnt <- 1
+  synList <- list()
+  nf <- length(synIDList)
+  message("Downloading files from Synapse...")
+  for(i in 1:nf){
+    syn <- synGet(synIDList[[i]], downloadFile=TRUE, downloadLocation=tempDir)
+    synList[[ names(synIDList)[i] ]] <- syn
+  }
+
+
+  for(as in alist){
+    message("Processing ", cnt , " / ", nas, " attractor cluster...")
+    if(class(as) != "AttractorSet") {
+    	cnt <- cnt + 1
+	next
+    }
+    clust <- as$attractors
+    na <- length(clust)
+    if(na < th) next
+    atlist <- list()
+    for(c in names(clust)){
+      tmp <- load(getFileLocation(synList[[c]]), env)[1]
+      x <- env[[tmp]]
+      topgene <- names(clust[[c]]$genes)[1]
+      idx <- which.max(x[,topgene])
+      atlist[[c]] <- x[idx,]
+    }
+    genes <- unique(unlist(lapply(atlist, function(x){names(x)})))
+    ng <- length(genes)
+    mat <- matrix(NA, nrow=ng, ncol=length(atlist))
+    rownames(mat) <- genes
+    colnames(mat) <- names(clust)
+
+    for(c in names(atlist)){
+      mat[names(atlist[[c]]), c] = atlist[[c]]
+    }
+
+    overlapSum <- unlist(lapply(as$attractors, function(a){sum(unlist(lapply(as$attractors, function(aa){aa$getOverlapNum(a)})))}))
+    overlapSum = sort(overlapSum, decreasing=T)
+    if(ncol(mat) > 2){
+      chosen <- names(overlapSum)[1:th]
+    }else{
+      chosen <- 1:ncol(mat)
+    }
+    cssMI <- apply(mat[,chosen], 1, mean)
+    cssMI <- sort(cssMI, decreasing=T)
+    cssMI <- cbind(cssMI[cssMI >= minScore])
+    if(length(cssMI) < minGenes) {
+	cnt <- cnt + 1
+	next
+    }
+    cssMI <- cbind(rownames(cssMI), cssMI)
+    rownames(cssMI) <- NULL
+    colnames(cssMI) <- c("Gene.Symbol", "Score")
+    attractome[[length(attractome)+1]] <- cssMI
+    names(attractome)[[length(attractome)]] <- cssMI[1,1]
+    cnt <- cnt + 1
+  }
+  if(outputMat){
+    cMat[[cssMI[1, 1]]] <- mat[cssMI[1:sz,1],]
+    return(list(attractome=attractome, consensusMat=cMat))
+  }
+  return (attractome)
+
+}
+
+
